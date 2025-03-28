71a0fa3314fcd3e05889042992d33ed92bb7c331
Katalin Molnar <molnarlin@gmail.com>
2025-03-28 21:19:54 +0000

    parent, child container

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
-import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
-import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+import NestingComponents from "./components/NestingComponents";
 
 function App() {
-  return (
-    <div className="App">
-      <ConditionalRenderingClass />
-      <ConditionalRenderingFunctional connected={true} />
-    </div>
-  );
+  return <div className="App">
+      <NestingComponents />
+  </div>;
 }
 
 export default App;

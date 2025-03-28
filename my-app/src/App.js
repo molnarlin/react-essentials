fc51e7203ba480d34bd74d3723d0d073990ef33c
Katalin Molnar <molnarlin@gmail.com>
2025-03-28 20:52:28 +0000

    conditional rendering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import EventsClass from "./components/EventsClass";
-import EventsFunctional from "./components/EventsFunctional";
+import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
 
 function App() {
-  return <div className="App">
-      <EventsFunctional />
-      <EventsClass />
-  </div>;
+  return (
+    <div className="App">
+      <ConditionalRenderingClass />
+      <ConditionalRenderingFunctional connected={true} />
+    </div>
+  );
 }
 
 export default App;

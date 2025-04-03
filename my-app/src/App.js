53905800a2c7827bae29174a50abccdfa9ab4550
Katalin Molnar <molnarlin@gmail.com>
2025-04-03 13:38:51 +0000

    effect cleanup functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import ClassCounter from "./components/ClassCounter";
-import UseEffectCounter from "./components/UseEffectCounter";
+import UseEffectCounterContainer from "./components/UseEffectCounterContainer";
 
 function App() {
   return <div className="App">
-      {/* <ClassCounter /> */}
-      <UseEffectCounter />
+      <UseEffectCounterContainer />
   </div>;
 }
 

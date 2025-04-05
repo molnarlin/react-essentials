5438180c7fc4f8f38860bd8cdcbbd583bb039ded
Katalin Molnar <molnarlin@gmail.com>
2025-04-05 11:57:18 +0000

    using API

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import UseEffectCounterContainer from "./components/UseEffectCounterContainer";
+import HTTPHooks from "./components/HTTPHooks";
 
 function App() {
   return <div className="App">
-      <UseEffectCounterContainer />
+      <HTTPHooks />
   </div>;
 }
 
